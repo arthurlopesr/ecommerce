778176b3e761edcac1d6e9363b1a7015e82d739c
Arthur Lopes <arthurlopr12@gmail.com>
2023-10-25 19:56:53 -0300

    chore: add database config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { UsersService } from './modules/users/users.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [UsersService],
 })
